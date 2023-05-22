9952096aa0d05b5ae3e91fb63106885bd0a8cc0c
DhanaSelvan J U <93440351+DhanaSelvan@users.noreply.github.com>
2023-05-22 21:33:45 +0530

    Add files via upload

---
+++ Day - 1 (Split and Join).py
@@ -0,0 +1,11 @@
+def split_and_join(line):
+    # write your code here
+    line = line.split(" ")
+    join = "-".join(line)
+    return join
+    
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
