1da6f1d5a6d260da26aa42d4958dab61b9811b03
DhanaSelvan J U <93440351+DhanaSelvan@users.noreply.github.com>
2023-07-06 17:12:17 +0530

    Add files via upload

---
+++ Minion game.py
@@ -0,0 +1,23 @@
+def minion_game(string):
+    string = string.upper()
+    length = len(string)
+    vowels = "AEIOU"
+    kevin_score = 0
+    stuart_score = 0
+    for i in range(length):
+        if string[i] in vowels:
+            kevin_score += length - i
+        else:
+            stuart_score += length - i
+            
+    if(stuart_score > kevin_score):
+        print(f"Stuart {stuart_score}")
+    elif(stuart_score < kevin_score):
+        print(f"Kevin {kevin_score}")
+    else:
+        print("Draw")
+    
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
